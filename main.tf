ed56737440fc527ede1a6c60544de0cc050fea6e
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-28 09:45:59 +0530

    new

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
+module "s3_bucket" {
+  source          = "./modules/s3"
+  bucket_name     = "sefali-banking-app-${random_id.bucket_suffix.hex}"
+  enable_versioning = true
+}
