4abf3334e1db8494d36d9533c0ee6a4d178a1da4
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-28 14:55:35 +0530

    updated-3

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.banking_app.id
-}
-
-output "website_url" {
-  description = "The website URL for the S3 bucket"
-  value       = aws_s3_bucket.banking_app.website_endpoint
-}
